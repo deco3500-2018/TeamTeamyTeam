2b67b097e2aa53d68099b09fa38d61554bcde97d
Kate Edwards <kateedwards012@gmail.com>
2018-10-04 12:43:24 +1000

    Ability to approve and decline posts

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+$(document).ready(function()
+
+{
+  $( ".accept" ).click(function() {
+    var image =   $(this).parent().prev(".img-post");
+    $(image).appendTo('#column2');
+    $(this).prev(".decline").remove();
+    $(this).remove();
+    $(image).clone().appendTo("#column3");
+  });
+
+
+  $( ".decline" ).click(function() {
+    var image = $(this).parent().prev(".img-post");
+    var accept = $(this).next(".accept");
+    var decline = $(this);
+
+
+    $(".popup, .popup-content").addClass("active");
+
+    $(".delete").click(function(){
+      $(".popup, .popup-content").removeClass("active");
+      $(image).fadeOut(400, function(){
+        $(this).remove();
+      });
+      $(decline).fadeOut(400, function(){
+        $(this).remove();
+      });
+      $(accept).fadeOut(400, function(){
+        $(this).remove();
+      });
+
+    });
+
+
+  });
+
+
+
+
+
+
+
+
+});
