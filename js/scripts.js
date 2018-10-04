42d680af4392291385dfe75ff19d5566894581b4
Kate Edwards <kateedwards012@gmail.com>
2018-10-04 21:11:40 +1000

    Posts are now hidden. Press 1 for Tiffany’s post and 2 for Bryan’s post.

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,38 @@ $(document).ready(function()
 {
 
 
+  $(document).keypress(function(e) {
+    if(e.which == 49) {
+        $("#tif-image").attr("src","img/tiffany.png");
+        $('#tiffany').removeClass("invisible");
+
+
+    };
+    if(e.which == 50) {
+      $("#bryan-image").attr("src","img/bryan.png");
+      $('#bryan').removeClass("invisible");
+    };
+});
+
   $( ".accept" ).click(function() {
     var image =   $(this).parent().prev(".img-post");
     $(image).appendTo('#column2, #column3').after(function(){
-      return "<div class='star-button' id='counter'><div class='star'><div class='material-icons star'>star</div></div><div class='likes'>0</div></div>";
+      return "<div class='cross-delete'>x</div><div class='star-button' id='counter'><div class='star'><div class='material-icons star'>star</div></div><div class='likes'>0</div></div>";
     });
     $(this).prev(".decline").remove();
     $(this).remove();
     $( ".star-button" ).click(function() {
       $('.likes').html(function(i, val) { return val*1+1 });
+    });
+    $( ".cross-delete" ).click(function(){
+      $(".popup, .popup-content").addClass("active");
+      $(".delete").click(function(){
+        $(".popup, .popup-content").removeClass("active");
+
+      });
+      $(".cancel").click(function(){
+        $(".popup, .popup-content").removeClass("active");
+      });
     });
   });
 
