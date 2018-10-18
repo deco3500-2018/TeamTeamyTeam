c1fc18071e0025595207021112d4f1bfb36ec424
Kate Edwards <kateedwards012@gmail.com>
2018-10-18 14:21:11 +1000

    fixed header and darker text in tabs

--- js/scripts.js
+++ js/scripts.js
@@ -127,6 +127,15 @@ $(document).ready(function()
 
   });
 
+  $(window).scroll(function(){
+    var sticky = $('.sticky'),
+        scroll = $(window).scrollTop();
+
+    if (scroll >= 100) sticky.addClass('fixed');
+    else sticky.removeClass('fixed');
+  });
+
+
 
 
 
